365a2033d89af77d367122efa45394b4388501fb
Rebard <31970194+Rebard@users.noreply.github.com>
2017-09-29 00:10:15 +0300

    Add files via upload

--- src/fsm.js
+++ src/fsm.js
@@ -8,7 +8,7 @@ class FSM {
 		 
 		 this.flag_success = false;
 		 this.flag_redo = false;
-		 this.history = [];	 
+		 this.history = [this.initial];	 
 		 this.index = 0;
 	}
 
@@ -19,8 +19,10 @@ class FSM {
     changeState(state) {
 		if(state in this.states)
 		{
-			this.history[this.index++] = this.activeState;
+			//this.history[this.index++] = this.activeState;
 			this.activeState = state; 
+			this.index++;
+			this.history[this.index] = this.activeState;
 			this.flag_success = true;
 		}
 		else
@@ -30,8 +32,9 @@ class FSM {
     trigger(event) {
 		if(event in this.states[this.activeState].transitions)
 		{
-			this.history[this.index++] = this.activeState;
 			this.activeState = this.states[this.activeState].transitions[event];
+			this.index++;
+			this.history[this.index] = this.activeState;
 			this.flag_success = true;
 		}
 		else throws("event don't exist");
@@ -57,34 +60,41 @@ class FSM {
 			}		
 		}
 		return arr;
-
 	}
 
     undo() {
-		if(this.activeState == this.initial)
-			return false;
-	
-		this.index --;
-		this.flag_redo = true;
+		console.log(this.history);
+		//if(this.activeState == this.initial)
+			if(this.history[this.index - 1] == undefined)
+				return false;
+		this.index--;
 		this.activeState = this.history[this.index];
+		if(this.flag_success)
+		{
+			this.flag_redo = true;
+			this.flag_success = false;
+			return true;
+		}
+	}
+
+    redo() {
+		console.log(this.history);
+		if(!this.flag_redo)
+		{
+			this.flag_redo = false;
+			return false;
+		}
+		this.index++;
+		if(this.history[this.index] == undefined)
+			return false;
+		this.activeState = this.history[this.index];
+		this.flag_success = true;
 		if(this.flag_success)
 		{
 			this.flag_success = false;
 			return true;
 		}
 		
-	}
-
-    redo() {
-		if(this.activeState == this.initial || !this.flag_redo)
-		{
-			this.flag_redo = false;
-			return false;
-		}
-				this.index++;
-				this.activeState = this.history[this.index];
-				if(this.flag_success)
-					return true;
 	}
 
     clearHistory() {
